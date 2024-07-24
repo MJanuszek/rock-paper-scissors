945359718714d40fe96764f9d05ea68dbdeffbe0
Monika Januszek <monika.januszek.mail@gmail.com>
2024-07-24 15:41:58 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import GameBoard from "./GameBoard";
+
+function App() {
+  return (
+    <>
+      <h1>Play new game</h1>
+      <GameBoard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/GameBoard.jsx
@@ -0,0 +1,18 @@
+import "./GameBoard.css";
+import { useState } from "react";
+
+function GameBoard() {
+  const [player1Hand, setPlayer1Hand] = useState();
+  const [player2Hand, setPlayer2Hand] = useState();
+  return (
+    <div className="container">
+      <div className="icons">
+        <div className="icon rock"></div>
+        <div className="icon paper"></div>
+        <div className="icon scissors"></div>
+      </div>
+    </div>
+  );
+}
+
+export default GameBoard;
