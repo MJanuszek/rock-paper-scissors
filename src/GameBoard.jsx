4a83b588c07369c6ea9bad0908f46a098a8e9b5c
Monika Januszek <monika.januszek.mail@gmail.com>
2024-07-29 12:18:22 +0200

    statistics added and styled

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -2,9 +2,18 @@ import "./GameBoard.css";
 import { useState } from "react";
 
 function GameBoard() {
-  const [player1Hand, setPlayer1Hand] = useState();
-  const [player2Hand, setPlayer2Hand] = useState();
-  const [nextPlayer, setNextPlayes] = useState(false);
+  const [player1Hand, setPlayer1Hand] = useState("");
+  const [player2Hand, setPlayer2Hand] = useState("");
+  // const [nextPlayer, setNextPlayes] = useState(false);
+  let oponentWins = 0;
+  let playerWins = 0;
+
+  function startNewGame() {
+    setPlayer1Hand("");
+    setPlayer2Hand("");
+  }
+
+  function oponentChoice() {}
 
   function handlePlayerChoice(choice) {
     console.log(choice);
@@ -16,14 +25,29 @@ function GameBoard() {
           className="icon rock"
           onClick={() => handlePlayerChoice("rock")}
         ></div>
-        <div className="icon paper"></div>
-        <div className="icon scissors"></div>
+        <div
+          className="icon paper"
+          onClick={() => handlePlayerChoice("paper")}
+        ></div>
+        <div
+          className="icon scissors"
+          onClick={() => handlePlayerChoice("scissors")}
+        ></div>
       </div>
       <div className="labels">
         <div className="label">Rock</div>
         <div className="label">Paper</div>
         <div className="label">Scissors</div>
       </div>
+      <div className="turn">
+        <h2>Player hand: {player1Hand} </h2>
+        <h2>Oponent hand: {player2Hand} </h2>
+      </div>
+      <div className="statistics">
+        <div>Player wins: {playerWins}</div>
+        <div>Oponent wins: {oponentWins}</div>
+      </div>
+      <button className="restart">Start new game</button>
     </div>
   );
 }
